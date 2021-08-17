5c35927e0ea50d4c24ab1ff87c8f88ba7190151b
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-08-16 18:47:47 -0700

    user-controller (add-friend) update-route built

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -80,6 +80,25 @@ const userController = {
           })
           .catch((err) => res.status(400).json(err));
       },
+
+      addFriend({ params }, res) {
+        User.findByIdAndUpdate(
+          { _id: params.id },
+          { $addToSet: { friends: params.friendId } },
+          { new: true }
+        )
+          .select("-__v")
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      },
     
 
 }
