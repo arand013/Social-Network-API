3f9509e75c25153032802bdb827ef6e7f92aac1a
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-08-16 18:39:38 -0700

    thought-controller (add/remove-reaction) update route built

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -65,5 +65,33 @@
             })
             .catch(err => res.json(err));
     },
+
+    addReaction({ params, body }, res) {
+        console.log("INCOMING BODY", body)
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true }
+        )
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
+    // remove reaction
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    }
   }
 module.exports = ThoughtController
