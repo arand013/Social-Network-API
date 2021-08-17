8afab0bb45c3381532f361ca4852475844a7ee98
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-08-17 00:17:28 -0700

    routes/ thought-route functions built

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -18,4 +18,21 @@ router
     .route('/:userId')
     .post(addThought);
 
+
+router
+.route('/:userId')
+.post(addThought);
+router
+.route('/:thoughtId')
+.get(getThoughtById)
+.put(addThought)
+.delete(removeThought)
+
+router
+// /api/thoughts/:thoughtId/reactions
+.route('/:thoughtId/reactions')
+.post(addReaction)
+.delete(removeReaction)
+
+
     module.exports = router;
