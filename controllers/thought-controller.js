c1dcf414aef4a56ba3c49d5dc20746da992213a2
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-08-16 18:35:14 -0700

    thought-controller getBy id route built

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -11,5 +11,21 @@
                 res.status(400).json(err);
             });
     },
+    getThoughtById({ params }, res) {
+        console.log("params sent", params)
+        Thought.findOne({ _id: params.thoughtId })
+      .select("-__v")
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+    },
   }
 module.exports = ThoughtController
