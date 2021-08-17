b63f4b90e886b9ea5e879939560d004570ebc1ad
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-08-16 18:42:46 -0700

    user-controller (One-user) getBy ID route built

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,6 +14,26 @@ const userController = {
                 res.status(400).json(err)
             })
     },
+    
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+          .populate({
+            path: "thoughts",
+            select: "-__v",
+          })
+          .select("-__v")
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
 }
 
     module.exports = userController
