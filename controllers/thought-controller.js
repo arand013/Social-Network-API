1e8c7578d41ec91ec94c33b7328f5fb0cddc0a2d
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-08-16 18:36:47 -0700

    thought-controller (add-thought)  post route built

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -27,5 +27,25 @@
         res.status(400).json(err);
       });
     },
+    // Route Add(s) a thought to User
+    addThought({ params, body }, res) {
+        console.log("INCOMING BODY", body)
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id! first error' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
   }
 module.exports = ThoughtController
